c2b5f1bb7317588d2b2863ad5c4339ff32901529
Rawanf2 <S240015388@seu.edu.sa>
2026-08-05 19:39:02 +0300

    Inserted sample data into Payment and Treatment_Medicine tables

---
+++ 05_InsertTreatment.sql
@@ -0,0 +1,7 @@
+INSERT INTO Treatment_Medicine VALUES (1, 1, 2);
+INSERT INTO Treatment_Medicine VALUES (1, 2, 1);
+INSERT INTO Treatment_Medicine VALUES (2, 4, 1);
+INSERT INTO Treatment_Medicine VALUES (3, 1, 1);
+INSERT INTO Treatment_Medicine VALUES (4, 4, 2);
+INSERT INTO Treatment_Medicine VALUES (4, 5, 1);
+INSERT INTO Treatment_Medicine VALUES (5, 2, 1);
