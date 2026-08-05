f6642d7bbcd67a9b83c432dc216959ed5a6364a9
Rawanf2 <S240015388@seu.edu.sa>
2026-08-05 19:09:02 +0300

    Inserted sample data into Patient and Doctor tables

---
+++ 03_InsertPatientDoctor.sql
@@ -0,0 +1,6 @@
+INSERT INTO Patient VALUES (1, 'M', 'ahmed.m@email.com', '2026-01-15');
+INSERT INTO Patient VALUES (2, 'F', 'sara.k@email.com', '2026-02-20');
+INSERT INTO Patient VALUES (4, 'F', 'noura.a@email.com', '2026-03-10');
+
+INSERT INTO Doctor VALUES (3, 'Cardiology', 'faisal.a@clinic.com', '2020-06-01');
+INSERT INTO Doctor VALUES (5, 'Dermatology', 'mansour.i@clinic.com', '2021-09-15');
