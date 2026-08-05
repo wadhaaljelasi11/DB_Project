a6ba3dc06d853c6bdc9f571b71ac8d83a6ff2de1
Rawanf2 <S240015388@seu.edu.sa>
2026-08-05 19:03:05 +0300

    Inserted sample data into Person table

---
+++ 02_InsertPerson.sql
@@ -0,0 +1,5 @@
+INSERT INTO Person VALUES (1, 'Ahmed', 'Mohammed', '0555123456', 'Riyadh', '1985-03-15');
+INSERT INTO Person VALUES (2, 'Sara', 'Khalid', '0555234567', 'Jeddah', '1990-07-22');
+INSERT INTO Person VALUES (3, 'Faisal', 'Abdullah', '0555345678', 'Dammam', '1978-11-10');
+INSERT INTO Person VALUES (4, 'Noura', 'Ali', '0555456789', 'Mecca', '1995-01-30');
+INSERT INTO Person VALUES (5, 'Mansour', 'Ibrahim', '0555567890', 'Medina', '1982-09-05');
